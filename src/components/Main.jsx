e379a580af2e2f71588de9c3e9c4cbb2399c9645
Allyson Mitchem <allysonmitchem@gmail.com>
2023-08-22 15:16:17 -0600

    added Main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react" 
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+
+const Main = () => {
+    return (
+        <Router>
+            <div id="main">
+
+            </div>
+        </Router>
+    )
+
+}
+
+export default Main
