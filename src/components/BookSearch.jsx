340896cdf2f60ce428ffd260ee809b847c8b824c
Allyson Mitchem <allysonmitchem@gmail.com>
2023-08-22 17:04:15 -0600

    added BookSearch component

---
+++ src/components/BookSearch.jsx
@@ -0,0 +1,16 @@
+import React from "react" 
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+
+const BookSearch= () => {
+    return (
+       
+            <div id="bookSearch">
+                <h2>Search me</h2>
+
+            </div>
+        
+    )
+
+}
+
+export default BookSearch
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import React from "react" 
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import {BookSearch} from './'
 
 const Main = () => {
     return (
         <Router>
             <div id="main">
                 <h2>HI.  It's Me.  I'm the problem.</h2>
-
+            <BookSearch/>
             </div>
         </Router>
     )
